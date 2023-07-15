57258da52e17b5195b281ade5013023ec79f8284
henriqueAvner <avnerhdpb@gmail.com>
2023-07-15 18:09:10 -0300

    fixed: corrigindo identação do arquivo getschedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,10 +1,6 @@
 const data = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => () => {
-  return data.species.filter((specie) => {
-    if (specie.name === scheduleTarget) {
-      return specie.availability;
-    }
-  });
+
 };
 module.exports = getSchedule;
