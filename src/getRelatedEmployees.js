4b0870e4c66392acae5307cc899ef502d8e677d8
henriqueAvner <avnerhdpb@gmail.com>
2023-07-14 23:46:45 -0300

    Feat: Requisito 4 do projeto

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  if (data.employees.some((employee) => employee.managers.includes(id))) {
+    return true;
+  }
+  return false;
 };
-
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((employee) => employee.managers.includes(managerId))
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
-
 module.exports = { isManager, getRelatedEmployees };
