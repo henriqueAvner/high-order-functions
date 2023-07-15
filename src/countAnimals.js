f6cc157d3dd2d110120d3f0b92cab73114bed5bb
henriqueAvner <avnerhdpb@gmail.com>
2023-07-15 00:51:21 -0300

    Feat: Requisito 5 do projeto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal || !animal.species) {
+    const obj = {};
+    data.species.forEach((species) => {
+      const count = species.residents.length;
+      obj[species.name] = count;
+    });
+    return obj;
+  }
+  const species = data.species.find((specie) => specie.name === animal.species);
+  if (!species) {
+    return 0;
+  }
+  if (animal.sex) {
+    const animalsFind = species.residents.filter((resident) => resident.sex === animal.sex).length;
+    return animalsFind;
+  }
+  const count = species.residents.length;
+  return count;
 };
 
 module.exports = countAnimals;
