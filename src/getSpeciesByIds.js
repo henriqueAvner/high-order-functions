cdae6756fab20512c9ac18cb506b0423679080b5
henriqueAvner <avnerhdpb@gmail.com>
2023-07-14 21:37:25 -0300

    feat: Requisito 1 do projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
-};
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
 
+  return data.species.filter((specie) => ids.includes(specie.id));
+};
 module.exports = getSpeciesByIds;
