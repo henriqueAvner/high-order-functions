f09f711260f88819e1a30c7c6f48e768438573ae
henriqueAvner <avnerhdpb@gmail.com>
2023-07-15 11:47:15 -0300

    Feat: Requisito 7 do projeto

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -12,7 +12,7 @@ describe('Testes da função HandlerElephants', () => {
   it('Retorna uma lista de nomes quando o parâmetro é "names"', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
-  it('Retorna o valor correto de elephants[param] quando o parametro é uma chave válida', () => {
+  it('Retorna o valor null de elephants[param] quando o parametro é uma chave válida', () => {
     const param = 'age';
     const result = handlerElephants(param);
     expect(result).toBe(null);
@@ -20,5 +20,11 @@ describe('Testes da função HandlerElephants', () => {
   it('Retorna o tamanho da array de elefantes quando é executado com o parâmetro count', () => {
     const result = handlerElephants('count');
     expect(result).toBe(4);
+  });
+  it('Retorna a localização do elefante quando se passa location como parâmetro', () => {
+    expect(handlerElephants('location').toBe('NW'));
+  });
+  it('Para o argumento averageAge, deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
 });
