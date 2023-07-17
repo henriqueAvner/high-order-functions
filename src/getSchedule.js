754a399d5b60f1b58cc50e70014074c14332675f
henriqueAvner <avnerhdpb@gmail.com>
2023-07-17 16:53:35 -0300

    fixed: Refatorando o requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,16 +4,15 @@ const { species, hours } = require('../data/zoo_data');
 const weekD = Object.keys(hours);
 
 const infosOfZoo = () => {
-  const obj = weekD.reduce((accumulator, currElement) => {
-    accumulator[currElement] = {
-      officeHour: `Open from ${hours[currElement].open}am until ${hours[currElement].close}pm`,
+  const obj = {};
+  for (const day of weekD) {
+    obj[day] = {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
       exhibition: species
-        .filter((animal) => animal.availability
-          .includes(currElement))
+        .filter((animal) => animal.availability.includes(day))
         .map((thisAnimal) => thisAnimal.name),
     };
-    return accumulator;
-  }, {});
+  }
   obj.Monday = {
     officeHour: 'CLOSED',
     exhibition: 'The zoo will be closed!',
@@ -22,16 +21,16 @@ const infosOfZoo = () => {
 };
 
 function getSchedule(scheduleTarget) {
-  if (!scheduleTarget) infosOfZoo();
-  const truthSpecies = species
-    .some((specie) => scheduleTarget === specie.name);
-  if (!truthSpecies && !weekD.includes(scheduleTarget)) {
+  if (!scheduleTarget) return infosOfZoo();
+  const isSpecies = species.some((specie) => scheduleTarget === specie.name);
+  const isDay = weekD.includes(scheduleTarget);
+  if (!isSpecies && !isDay) {
     return infosOfZoo();
   }
-  if (weekD.includes(scheduleTarget)) {
+  if (isDay) {
     return { [scheduleTarget]: infosOfZoo()[scheduleTarget] };
   }
-
   return species.find((dispoSpecie) => dispoSpecie.name === scheduleTarget).availability;
 }
+
 module.exports = getSchedule;
