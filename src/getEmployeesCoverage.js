d41ded5f251715bcea88f29069cf07cafca3246e
henriqueAvner <avnerhdpb@gmail.com>
2023-07-17 17:01:21 -0300

    fixed: Requisito 11 - refatorando getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -6,22 +6,31 @@ const resultArr = employees.map((thisObj) => {
   const convertedObj = {
     id: thisObj.id,
     fullName: `${thisObj.firstName} ${thisObj.lastName}`,
-    species: thisObj.responsibleFor.map((id) => species
-      .find((specie) => specie.id === id).name),
-    locations: thisObj.responsibleFor.map((id) => species
-      .find((specie) => specie.id === id).location),
+    species: thisObj.responsibleFor.map((id) => {
+      const specieName = species.find((specie) => specie.id === id);
+      return specieName ? specieName.name : null;
+    }),
+    locations: thisObj.responsibleFor.map((id) => {
+      const specieLocation = species.find((specie) => specie.id === id);
+      return specieLocation ? specieLocation.location : null;
+    }),
   };
   return convertedObj;
 });
 
 const getEmployeesCoverage = (param) => {
   if (param === undefined) return resultArr;
-  const checkEntry = resultArr
-    .find((element) => element.fullName.includes(Object.values(param))
-    || element.id.includes(Object.values(param)));
+
+  const checkEntry = resultArr.find((element) => (
+    element.fullName.includes(Object.values(param))
+      || element.id.includes(Object.values(param))
+  ));
+
   if (checkEntry === undefined) {
     throw new Error('Informações inválidas');
   }
+
   return checkEntry;
 };
+
 module.exports = getEmployeesCoverage;
